d1ebaa4ad9d3961b035a24b9ca2b1069e835dd8d
Alejandro Jimenez <alejjime@c2r4s3.42warsaw.pl>
2024-12-03 18:20:00 +0100

    Terminado hasta ft_memset.c

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,21 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:56:53 by alejjime          #+#    #+#             */
-/*   Updated: 2024/12/03 15:57:49 by alejjime         ###   ########.fr       */
+/*   Updated: 2024/12/03 18:16:51 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	*ft_memset(void *str, int c, unsigned int n)
+{
+	unsigned int	i;
+	unsigned char	*s;
+
+	s = (unsigned char *)str;
+	i = 0;
+	while (i < n)
+	{
+		s[i] = (unsigned char)c;
+		i++;
+	}
+	return (str);
+}
--- Libft/main.c
+++ Libft/main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int	ft_isalpha(int a);
-int	ft_isdigit(int d);
-int	ft_isalnum(int an);
-int	ft_isascii(int as);
-int	ft_isprint(int pr);
-int	ft_strlen(char *s);
+int		ft_isalpha(int a);
+int		ft_isdigit(int d);
+int		ft_isalnum(int an);
+int		ft_isascii(int as);
+int		ft_isprint(int pr);
+int		ft_strlen(char *s);
+void	*ft_memset(void *str, int c, unsigned int n);
 
 int	main(void)
 {
-    //ALPHA
+	//ALPHA
 	printf("Is alpha: %i TRUE\n", ft_isalpha('g'));
-    printf("Is alpha: %i FALSE\n\n", ft_isalpha('0'));
-    //DIGIT
-    printf("Is digit: %i TRUE\n", ft_isdigit('9'));
-    printf("Is digit: %i FALSE\n\n", ft_isdigit('*'));
-    //ALPHANUMERIC
-    printf("Is alnum: %i TRUE\n", ft_isalnum('i'));
-    printf("Is alnum: %i TRUE\n", ft_isalnum('4'));
-    printf("Is alnum: %i FALSE\n\n", ft_isalnum(')'));
-    //ASCII
-    printf("Is ascii: %i TRUE\n", ft_isascii('H'));
-    printf("Is ascii: %i FALSE\n\n", ft_isascii(143)); //Å
-    //PRINTABLE
-    printf("Is printable: %i TRUE\n", ft_isprint(' '));
-    printf("Is printable: %i FALSE\n\n", ft_isprint('\n'));
-    //STRING LENGHT
-    printf("String lenght: %i \n", ft_strlen("hol4s"));
-    printf("String lenght: %i \n\n", ft_strlen("turtle"));
+	printf("Is alpha: %i FALSE\n\n", ft_isalpha('0'));
+	//DIGIT
+	printf("Is digit: %i TRUE\n", ft_isdigit('9'));
+	printf("Is digit: %i FALSE\n\n", ft_isdigit('*'));
+	//ALPHANUMERIC
+	printf("Is alnum: %i TRUE\n", ft_isalnum('i'));
+	printf("Is alnum: %i TRUE\n", ft_isalnum('4'));
+	printf("Is alnum: %i FALSE\n\n", ft_isalnum(')'));
+	//ASCII
+	printf("Is ascii: %i TRUE\n", ft_isascii('H'));
+	printf("Is ascii: %i FALSE\n\n", ft_isascii(143)); //Å
+	//PRINTABLE
+	printf("Is printable: %i TRUE\n", ft_isprint(' '));
+	printf("Is printable: %i FALSE\n\n", ft_isprint('\n'));
+	//STRING LENGHT
+	printf("String lenght: %i \n", ft_strlen("hol4s"));
+	printf("String lenght: %i \n\n", ft_strlen("turtle"));
+	//MEMORY SET
+	char s[12] = "Hola a todos";
+	int n[4] = {1, 2, 3, 4};
+	printf("String before: %s \n", s);
+	ft_memset(s, 'A', 4);
+	printf("String after: %s \n\n", s);
 }
