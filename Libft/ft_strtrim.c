29d540638445928b2f6b2c26fdcbbc3fbf5f594d
alejjime <alejandrojimenez2718@gmail.com>
2024-12-31 21:31:39 +0000

    Hecho lamitad de strtrim

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 17:08:45 by alejjime          #+#    #+#             */
-/*   Updated: 2024/12/29 20:40:43 by alejjime         ###   ########.fr       */
+/*   Updated: 2024/12/31 21:26:33 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,36 @@
 */
 #include "libft.h"
 
+int	set_in_string(char letter, char *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (letter == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*temp;
 	int		i;
 	int		j;
 	int		start;
+	int		end;
 	int		set_len;
 
 	if (!s1 || !set)
 		return (NULL);
 	start = 0;
-
-	
-	temp = (char *)&s1[start];
-	return (temp);
+	i = 0;
+	end = 0;
+	while (set_in_string(s1[start], set) == 1)
+		start++;
+	while (set_in_string(s1[start + i], set) != 1)
+		i++;
 }
