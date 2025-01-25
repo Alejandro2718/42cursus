be4b0edb666d86c608226389e78fa5d7de599c52
Alejandro Jimenez <alejjime@c1r7s3.42warsaw.pl>
2025-01-25 16:43:05 +0100

    Inicio de printf y haciendo tests

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.c                                      :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 11:49:10 by alejjime          #+#    #+#             */
-/*   Updated: 2025/01/24 14:54:25 by alejjime         ###   ########.fr       */
+/*   Updated: 2025/01/25 16:23:14 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 */
 #include "libftprintf.h"
 
-int	ft_printf(const char *, ...)
+void	print_char(char a)
 {
+	write(1, &a, 1);
+}
+
+int	ft_printf(const char *string, ...)
+{
+	int	i;
+
+	i = 0;
+	if (string)
+	{
+		while (string[i] != '\0')
+		{
+			print_char(string[i]);
+			i++;
+		}
+	}
+	return (i);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,17 +6,17 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 11:50:07 by alejjime          #+#    #+#             */
-/*   Updated: 2025/01/24 14:54:20 by alejjime         ###   ########.fr       */
+/*   Updated: 2025/01/24 16:27:31 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *string, ...);
 
 #endif
