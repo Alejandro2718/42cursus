a337b5fc9993a7f6072786d4fe2bf82a574202d3
Alejandro Jimenez <alejjime@c1r7s1.42warsaw.pl>
2025-03-10 20:28:49 +0100

    continue with minitalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,16 +6,18 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 16:40:45 by alejjime          #+#    #+#             */
-/*   Updated: 2025/03/09 16:01:57 by alejjime         ###   ########.fr       */
+/*   Updated: 2025/03/10 16:57:35 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/libft.h"
+#include <signal.h>
 
 int	main(int argc, char **argv)
 {
-	for (int i = 1; i < argc; i++)
-	{
-		ft_printf("%s\n", argv[i]);
-	}
+	if (argc != 3 || !argv[2])
+		return (1);
+	ft_printf("Server PID: %i\n", ft_atoi(argv[1]));
+	ft_printf("Message sent: %s\n", argv[2]);
+	kill((int)argv[1][0], SIGUSR1);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,19 +6,33 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 16:40:42 by alejjime          #+#    #+#             */
-/*   Updated: 2025/03/09 16:34:15 by alejjime         ###   ########.fr       */
+/*   Updated: 2025/03/10 16:57:04 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/libft.h"
 #include <signal.h>
 
+void	get_message_from_client(int sig)
+{
+	volatile sig_atomic_t	signal_code;
+
+	signal_code = sig;
+	ft_printf("message from client received: \n");
+}
+
 int	main(void)
 {
 	int	server_pid;
 
+	signal(SIGUSR1, get_message_from_client);
+	signal(SIGUSR2, get_message_from_client);
+	signal(SIGTERM, get_message_from_client);
 	server_pid = getpid();
 	ft_printf("PID: %i\n", server_pid);
-	// kill(pid);
-	ft_printf("end\n");
+	while (1)
+	{
+		pause();
+	}
+	return (0);
 }
