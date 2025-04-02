7c9a4eac0a6688f43e4bcb23e8daa70bd7bd20fd
Alejandro Jimenez <alejjime@c1r3s3.42warsaw.pl>
2025-04-02 17:34:53 +0200

    only change the global variables logic

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,7 +6,7 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 16:40:45 by alejjime          #+#    #+#             */
-/*   Updated: 2025/03/10 16:57:35 by alejjime         ###   ########.fr       */
+/*   Updated: 2025/04/02 17:25:48 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,51 @@
 #include <signal.h>
 #include <unistd.h>
 
-void print_binary(int pid, unsigned char ch) {
-    for (int i = 7; i >= 0; i--) {
-        // Send signal based on bit value (0 or 1)
-        int bit_signal = (ch & (1 << i)) ? 1 : 0;
-        
-        if (bit_signal == 1) {
-            kill(pid, SIGUSR1);  // Signal for bit 1
-        } else {
-            kill(pid, SIGUSR2);  // Signal for bit 0
-        }
-        
-        // Add a small delay between signals to ensure they are processed
-        usleep(100);  // 100ms delay
-    }
+void	print_binary(int pid, unsigned char ch)
+{
+	int	bit_signal;
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if ((ch & (1 << i)) != 0)
+		{
+			bit_signal = 1;
+			kill(pid, SIGUSR1);
+		}
+		else
+		{
+			bit_signal = 0;
+			kill(pid, SIGUSR2);
+		}
+		usleep(1000);
+		i--;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc != 3 || !argv[2]) {
-        ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
-        return (1);
-    }
+	char *message;
+	int server_pid;
 
-    int server_pid = ft_atoi(argv[1]);
-    char *message = argv[2];
+	if (argc != 3 || !argv[2])
+	{
+		ft_printf("Usage: %s <server_pid> <message>\n", argv[0]);
+		return (1);
+	}
 
-    ft_printf("Server PID: %i\n", server_pid);
-    ft_printf("Message sent: %s\n", message);
+	server_pid = ft_atoi(argv[1]);
+	message = argv[2];
 
-    // Send each character of the message
-    while (*message) {
-        print_binary(server_pid, *message);
-        message++;
-    }
-    
-    return (0);
+	ft_printf("Server PID: %i\n", server_pid);
+	ft_printf("Message sent: %s\n", message);
+
+	while (*message)
+	{
+		print_binary(server_pid, *message);
+		message++;
+	}
+
+	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: alejjime <alejjime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 16:40:42 by alejjime          #+#    #+#             */
-/*   Updated: 2025/03/10 16:57:04 by alejjime         ###   ########.fr       */
+/*   Updated: 2025/04/02 17:34:03 by alejjime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,44 @@
 #include <signal.h>
 
 // Global variables to keep track of bits
-unsigned char g_current_byte = 0;
-int g_bits_received = 0;
+unsigned char	g_current_byte = 0;
+int				g_bits_received = 0;
 
-void handle_bit(int bit_value)
+void	handle_signal(int signum)
 {
-    // Shift the current byte left and add the new bit
-    g_current_byte = (g_current_byte << 1) | bit_value;
-    g_bits_received++;
-    
-    // If we have received 8 bits (a complete byte)
-    if (g_bits_received == 8)
-    {
-        // Print the character
-        ft_printf("Character received: %c\n", g_current_byte);
-        
-        // Reset for the next character
-        g_current_byte = 0;
-        g_bits_received = 0;
-    }
-}
+	int	bit_value;
 
-void get_1(int sig)
-{
-    if (sig == SIGUSR1)
-    {
-        // ft_printf("Bit received: 1\n");
-        handle_bit(1);
-    }
-}
-
-void get_0(int sig)
-{
-    if (sig == SIGUSR2)
-    {
-        // ft_printf("Bit received: 0\n");
-        handle_bit(0);
-    }
+	if (signum == SIGUSR1)
+		bit_value = 1;
+	else
+		bit_value = 0;
+	g_current_byte = (g_current_byte << 1) | bit_value;
+	g_bits_received++;
+	if (g_bits_received == 8)
+	{
+		// ft_printf("%c", g_current_byte);
+		ft_putchar_fd(g_current_byte, 1);
+		g_current_byte = 0;
+		g_bits_received = 0;
+	}
 }
 
 int	main(void)
 {
-	int	server_pid;
+	int					server_pid;
+	struct sigaction	sa;
 
-	struct sigaction action_1, action_2;
-	action_1.sa_handler = get_1;
-	action_2.sa_handler = get_0;
-  	sigemptyset(&action_1.sa_mask);
-	sigemptyset(&action_2.sa_mask);
-  	action_1.sa_flags = 0;
-	action_2.sa_flags = 0;
-
-	sigaction(SIGUSR1, &action_1, NULL);
-	sigaction(SIGUSR2, &action_2, NULL);
-
+	sa.sa_handler = handle_signal;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) ==
+		-1)
+	{
+		ft_printf("Error setting up signal handlers\n");
+		return (1);
+	}
 	server_pid = getpid();
 	ft_printf("PID: %i\n", server_pid);
 	while (1)
